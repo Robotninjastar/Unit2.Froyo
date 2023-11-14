8d9e6e25876bd97a7e6e120b40d3055cc8872ebe
Robotninjastar <karonbrown06@gmail.com>
2023-11-13 20:09:49 -0700

    froyo counter

--- froyo.js
+++ froyo.js
@@ -1,10 +1,32 @@
 //TODO
+const froyo = { 
+    Choclate: 0,
+    Vanilla: 0,
+    Strawberry: 0,
+    Coffee: 0,
+  };
 
-function myFunction() {
-    let person = prompt("Please enter your Flavors");
-    if (person != null) {
+const userFlavorstring = prompt(
+  "Please enter Froyo Flavors separate them with commas. Choices are Choclate,Strawberry,Vanilla and Coffee.",
+  "Choclate,Vanilla,Strawberry"
+);
 
-     
+const stringArray = userFlavorstring.split(",");
 
-    }
-  }
+const flavors = [];
+
+for (let i = 0; i < stringArray.length; i++) {
+
+  const flav = stringArray[i];
+
+  console.log(i,flav,froyo[flav])
+  froyo[flav]++
+
+  console.log(froyo)
+}
+
+
+
+
+
+
